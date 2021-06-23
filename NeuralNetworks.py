5e192d5e23ddda4cbf3b3d6d5c624e4d6b0819aa
Felipe Quezada <kezada66@gmail.com>
2021-06-23 14:29:05 -0400

    added calibration plot

--- NeuralNetworks.py
+++ NeuralNetworks.py
@@ -15,30 +15,27 @@ class Lenet5Deterministic(nn.Module):
         ## Kernel size changed to 4 to fit data (original Lenet was designed for 32x32x1 imgs)
         self.conv3 = nn.Conv2d(16, 120, 4, stride=1)
 
-        self.fc1 = nn.Linear(120, 84)  
+        self.fc1 = nn.Linear(120, 84) 
+        
+        ## One output interpreted as the probability of being class 1
         self.fc2 = nn.Linear(84, 1)
         
         self.activation = nn.Tanh()
+        
+        ## Probability as output
         self.output = nn.Sigmoid()
         
     def forward(self, x):
-        #print(x.shape)
         x = F.avg_pool2d(self.activation(self.conv1(x)), 2, stride=2)
-        #print(x.shape)
         x = F.avg_pool2d(self.activation(self.conv2(x)), 2, stride=2)
-        #print(x.shape)
         x = self.activation(self.conv3(x))
-        #print(x.shape)
+
         x = torch.flatten(x, 1) # flatten all dimensions except the batch dimension
-        #print(x.shape)
+        
         x = self.activation(self.fc1(x))
-        #print(x.shape)
-                
         x = self.output(self.fc2(x))
-        #x = torch.tanh(self.fc2(x))
         
         x = x.squeeze(1)
-
         return x
     
 class Lenet5Bayesian(PyroModule):
@@ -47,6 +44,7 @@ class Lenet5Bayesian(PyroModule):
         prior = dists.Normal(0, prior_scale)
         priorFE = dists.Normal(0, prior_scaleFE)
         
+        ## Make the feature extraction part of the NN bayesian?
         if isFEBayesian:
             self.conv1 = PyroModule[nn.Conv2d](1, 6, 5, stride=1)
             self.conv1.weight = PyroSample(priorFE.expand([6, 1, 5, 5]).to_event(4))
@@ -74,6 +72,7 @@ class Lenet5Bayesian(PyroModule):
         self.fc2.weight = PyroSample(prior.expand([1, 84]).to_event(2))
         self.fc2.bias = PyroSample(prior.expand([1]).to_event(1))
         
+        ## Same as before, one value as output interpreted as a probability
         self.output = nn.Sigmoid()
         self.activation = nn.Tanh()
 
@@ -93,6 +92,6 @@ class Lenet5Bayesian(PyroModule):
         #print(f.shape)
         with pyro.plate("data", f.shape[0]):
             loc = pyro.deterministic("k", f, event_dim=0)   
-            obs = pyro.sample("obs", dists.Bernoulli(probs=loc), obs=y) #likelihood
+            obs = pyro.sample("obs", dists.Bernoulli(probs=loc), obs=y) #Bernoulli likelihood
         
         return f
